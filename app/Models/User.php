b83c9fe285cf14e1741a49f7e9422adfbb126f35
King <King.wang@danielwellington.com>
2019-03-04 14:59:08 +0800

    用户信息展示

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,4 +37,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /*
+     * get user icon
+     * @return string
+     */
+    public function gravatar($size = 100)
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
