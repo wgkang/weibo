60cc681340f2095c6d4ab6601ecf82949a9c39b9
King <King.wang@danielwellington.com>
2019-03-04 18:00:07 +0800

    登录注册完成

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use  Auth;
 
 class SessionsController extends Controller
 {
@@ -11,12 +12,30 @@ class SessionsController extends Controller
         return view('sessions.create');
     }
 
+    /**
+     * @param Request $request
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function store(Request $request)
     {
         $param = $this->validate($request,[
-            'email'=>'requried|email|max:255',
-            'password'=>'requried'
+            'email'=>'required|email|max:255',
+            'password'=>'required'
         ]);
 
+        if (Auth::attempt($param, $request->has('remember'))){
+            session()->flash('success','欢迎回来');
+            return redirect()->route('users.show',[Auth::user()]);
+        }else{
+            session()->flash('danger','很抱歉，邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success','您已退出登录。');
+        return redirect('/');
     }
 }
